baca67820284b5009d40b3b9fb58e619532b9219
ExoFake <albertomoltrasio13@proton.me>
2025-12-13 14:21:57 +0000

    Primer commit

---
+++ src/main/java/com/novastock/api_novastock/ApiNovastockApplication.java
@@ -0,0 +1,13 @@
+package com.novastock.api_novastock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNovastockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNovastockApplication.class, args);
+	}
+
+}
